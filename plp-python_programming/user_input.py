5f928cd2ee1e4c60e4854a1bb9719b376846276b
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2024-03-05 15:10:04 +0300

    commit

---
+++ plp-python_programming/user_input.py
@@ -0,0 +1,5 @@
+#a program that asks the user for their name, age, and location and then prints out a personalized message
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+print("Your name is " +name+ ",aged " +age+ " and I'm from ", location)
